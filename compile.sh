e751504fa2185b0ecb057a504d69783c14dec7ba
elvout <elvin.yang9@gmail.com>
2019-01-07 20:33:47 -0600

    update build files

--- compile.sh
+++ compile.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env bash
 
-sed -E -i '' "s/Compiled 20[0-9]{2}-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9] [AP]M/Compiled $(date '+%F %r')/" $1.lua
+sed -E -i '' "s/Compiled 20[0-9]{2}-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9] [AP]M/Compiled $(date '+%F %r')/" term.lua
 
-cat $1.lua > $1COMPILE.lua
+cat term.lua > termCOMPILE.lua
 
 for f in *.lua; do
-	if [[ "$f" != $1*.lua ]]; then
-		cat $f >> $1COMPILE.lua
+	if [[ "$f" != term*.lua ]]; then
+		cat $f >> termCOMPILE.lua
 	fi		
 done
 
-luna $1COMPILE.lua $1.tns
+luna termCOMPILE.lua term.tns
 
-rm $1COMPILE.lua
+rm termCOMPILE.lua
